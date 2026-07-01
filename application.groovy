868287dc58eda993c3b2b518ae72622ab02d9049
Aakanksha Shisode <shisodeaakanksha@gmail.com>
2026-07-01 14:22:55 +0530

    fnal-pipeline

--- application.groovy
+++ application.groovy
@@ -29,8 +29,8 @@ pipeline{
 		steps{
 		   withCredentials([
 		    usernamePassword(
-		      credentialsId: 'dockerhub'
-		      usernameVariable:'DOCKER_USER'
+		      credentialsId: 'dockerhub',
+		      usernameVariable:'DOCKER_USER',
 		      passwordVariable:'DOCKER_PASS'
 		    )
 		   ]){
